320b92be960b4414c9e87362a7d268afd5710437
Maxwell G Kofford <maxkofford@gmail.com>
2016-09-13 14:16:09 -0600

    testpy

--- ballgame/Assets/script/moveball.cs
+++ ballgame/Assets/script/moveball.cs
@@ -3,6 +3,42 @@ using System.Collections;
 //using UnityEngine.UI;
 
 public class moveball : MonoBehaviour {
+
+    public static void testPython()
+    {
+        Debug.Log("*********************************TEEEEEEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSTTTTTTTTTTTTTTTTT");
+        
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = "/C copy /b Image1.jpg + Archive.rar Image2.jpg";
+        process.StartInfo = startInfo;
+        process.Start();
+        string output = p.StandardOutput.ReadToEnd();
+ p.WaitForExit();
+
+        Debug.Log(output);
+ /*
+
+        // Start the child process.
+ Process p = new Process();
+ // Redirect the output stream of the child process.
+ p.StartInfo.UseShellExecute = false;
+ p.StartInfo.RedirectStandardOutput = true;
+ p.StartInfo.FileName = "YOURBATCHFILE.bat";
+ p.Start();
+ // Do not wait for the child process to exit before
+ // reading to the end of its redirected stream.
+ // p.WaitForExit();
+ // Read the output stream first and then wait.
+ string output = p.StandardOutput.ReadToEnd();
+ p.WaitForExit();
+
+        */
+    }
+
+
     public float speed;
    // public Text countText;
    // public Text winText;
